6601ed742ec56442c38b7a9d37fe13379f4b4196
Yauheni Pasiukevich <pasyukevich@live.com>
2023-08-20 21:08:32 +0200

    feat: add env variables

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,8 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      'dotenv-import',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: true,
-      },
+      'module:react-native-dotenv',
+      {moduleName: 'react-native-dotenv', path: '.env'},
     ],
   ],
 };
---
+++ src/types/env.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-native-dotenv' {
+  export const API_KEY: string;
+  export const API_URL: string;
+  export const BASE_URL: string;
+}
